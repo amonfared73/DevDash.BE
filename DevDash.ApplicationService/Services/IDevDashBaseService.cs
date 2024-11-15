87d472c523970e60b6d3d3aec8c0d744d1e4327e
Amir Monfared <amonfared73@gmail.com>
2024-11-15 11:58:46 +0330

    introduce IDevDashBaseService

---
+++ DevDash.ApplicationService/Services/IDevDashBaseService.cs
@@ -0,0 +1,15 @@
+﻿using DevDash.Domain.BaseModels;
+using System.Linq.Expressions;
+
+namespace DevDash.ApplicationService.Services
+{
+    public interface IDevDashBaseService<T> where T : DashObject
+    {
+        Task<IQueryable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<IQueryable<T>> FindAsync(Expression<Func<T,bool>> predicate);
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
